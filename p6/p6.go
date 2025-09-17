d344417461265ef92912af9a472581660a3b6fa9
Joanna Górska <jgorska@AsiaBook.local>
2025-09-17 19:08:40 +0200

    Implementing solution for problem 6 of leetcode challange: Zigzag string convertion

--- p6/p6.go
+++ p6/p6.go
@@ -5,5 +5,40 @@ func Run(s string, numRows int) string {
 }
 
 func convert(s string, numRows int) string {
-	return ""
+	if numRows == 1 || len(s) == 0 {
+		return s
+	}
+	retByteSlice := []byte{}
+
+	verticalCount := numRows
+	diagonalCount := numRows - 2
+
+	offset := verticalCount + diagonalCount
+
+	for row := 0; row < numRows; row++ {
+		id := row
+
+		vertical := true
+
+		diagonalOffset := offset - row*2
+		verticalOffset := offset - diagonalOffset
+
+		for id < len(s) {
+			retByteSlice = append(retByteSlice, s[id])
+
+			if row == 0 || row == numRows-1 {
+				id += offset
+			} else {
+				if vertical {
+					id += diagonalOffset
+				} else {
+					id += verticalOffset
+				}
+			}
+
+			vertical = !vertical
+		}
+	}
+
+	return string(retByteSlice)
 }
