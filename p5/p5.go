b4e79f99cc4a3a08932ee1019c46adac2c220e31
Joanna Górska <jgorska@AsiaBook.local>
2025-09-17 14:04:43 +0200

    Implementing solution for problem 5 by expanding search from every character in s for both even and uneven palindromes

--- p5/p5.go
+++ p5/p5.go
@@ -1,12 +1,46 @@
 package p5
 
+import "strings"
+
 func Run(s string) string {
 	return longestPalindrome(s)
 }
 
 func longestPalindrome(s string) string {
 	maxLen := 0
-	palindromeMap := map[int]string{}
+	retPalindrome := ""
 
-	return palindromeMap[maxLen]
+	start := 0
+	end := 0
+
+	splitString := strings.Split(s, "")
+
+	for key, _ := range splitString {
+		left, right := key, key
+
+		for left >= 0 && right < len(splitString) && splitString[left] == splitString[right] {
+			if (right - left + 1) > maxLen {
+				maxLen = right - left + 1
+				start = left
+				end = right
+			}
+			left -= 1
+			right += 1
+		}
+
+		left, right = key, key+1
+		for left >= 0 && right < len(splitString) && splitString[left] == splitString[right] {
+			if (right - left + 1) > maxLen {
+				maxLen = right - left + 1
+				start = left
+				end = right
+			}
+			left -= 1
+			right += 1
+		}
+	}
+
+	retPalindrome = strings.Join(splitString[start:end+1], "")
+
+	return retPalindrome
 }
