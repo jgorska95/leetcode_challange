8af827a155d19011170e66ebc3c5d13d1aab2c30
Joanna Górska <jgorska@AsiaBook.local>
2025-09-18 14:06:54 +0200

    Initial commit for leetcode problem 7: reverse integer

---
+++ p7/p7.go
@@ -0,0 +1,9 @@
+package p7
+
+func Run(x int) int {
+	return reverse(x)
+}
+
+func reverse(x int) int {
+	return 0
+}
---
+++ p7/p7_test.go
@@ -0,0 +1,42 @@
+package p7_test
+
+import (
+	"testing"
+
+	"main.go/p7"
+)
+
+/*
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+*/
+
+type DataSet struct {
+	input  int
+	output int
+}
+
+func Test(t *testing.T) {
+	testSet := []DataSet{
+		{123, 321},
+		{-123, -321},
+		{120, 21},
+	}
+
+	for testKey, testVal := range testSet {
+		tmp := p7.Run(testVal.input)
+		if tmp != testVal.output {
+			t.Error("Test case failed at test nr: ", testKey)
+			break
+		}
+	}
+}
